59fa327892effe5d901f8fd9b98562ef0459804c
bforrester722 <b.forrester@live.com>
2021-01-29 11:52:52 -0600

    removes redundant input classes

--- src/app/input/input-textfield.ts
+++
@@ -1,6 +0,0 @@
-import { InputBase } from './input-base';
-// adds controlType textbox, used in input.service 
-export class InputTextfield extends InputBase<string> {
-  controlType = 'textbox';
-}
-
